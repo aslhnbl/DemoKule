89a0dccd8b1fa5f345704236a1c88de5ffe816e6
aslhnbl <aslihanbal06@gmail.com>
2021-09-03 16:17:05 +0300

    initial commit
    Bu bir başlangıç commit idir.

---
+++ Saat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoKule
+{
+    public class Saat
+    {
+        public string value { get; set; }
+
+        public string text { get; set; }
+
+
+    }
+}
